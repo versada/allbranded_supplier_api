e10ab5992b089025563cdf84ded39550c5772c82
Tomas Racys <tomas@versada.eu>
2018-02-19 15:50:51 +0200

    Make <DecoImageURL/> and <HexCode/> optional.

--- xml_feeds_schema/__openerp__.py
+++ xml_feeds_schema/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'XML Feeds Schema',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.0.1',
     'author': 'Versada',
     'category': 'Connector',
     'website': 'https://versada.eu',
